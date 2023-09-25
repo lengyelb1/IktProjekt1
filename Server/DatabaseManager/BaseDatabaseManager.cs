c26634510da0651ab3e682c44bde6a0837db5afd
Lengyel Bálint <112940758+lengyelb1@users.noreply.github.com>
2023-09-25 12:01:57 +0200

    Add files via upload

---
+++ Server/DatabaseManager/BaseDatabaseManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Server.DatabaseManager
+{
+    public class BaseDatabaseManager
+    {
+        protected BaseDatabaseManager() { }
+
+        public static MySqlConnection connection
+        {
+            get
+            {
+                MySqlConnection connection = new MySqlConnection();
+                string connctionString = "SERVER=192.168.50.90;"+"DATABASE=weblap;"+"UID=root;"+"PASSWORD=password;"+"SSL MODE=none;";
+                connection.ConnectionString = connctionString;
+                return connection;
+            }
+        }
+    }
+}
---
+++ Server/DatabaseManager/IDQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DatabaseManager
+{
+    internal interface IDQL
+    {
+        string Insert(Record record);
+
+        string Update(Record record);
+
+        string Delete(Record record);
+    }
+}
